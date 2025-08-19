337d7128fb471acf22c3e8e00912e9b6fe7b6bbb
Alonso0729 <Eason.Zhang0731@outlook.com>
2025-08-19 16:09:16 +0800

    Update on 20250819 part 10

--- Scripts/js/HongKong/PhoenixTV.js
+++ Scripts/js/HongKong/PhoenixTV.js
@@ -5,7 +5,7 @@ function main(item) {
         'fhzx': 'info', // 凤凰资讯
         'fhhk': 'hk'   // 凤凰香港
     };
-    const token = "id=cn&prefix=86&phone=13256889895&pwd=Fan2345678";//备用号码:phone=13389247903&pwd=Llxxcc198
+    const token = "id=cn&prefix=86&phone=13256889895&pwd=Fan2345678";//备用号码1:phone=13389247903&pwd=Llxxcc198,备用号码2:phone=13955036885&pwd=make123456MAKE
     const targetUrl = `http://tv.groupshare.com.cn/fhtv?id=${n[id]}&token=${token}`;
     return JSON.stringify({
         url: targetUrl
